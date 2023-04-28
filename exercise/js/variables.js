e7206b081069e7d2781d6da9ceb50d127bfa83d8
sensei_blaq <jnr.amamoo@gmail.com>
2023-04-28 20:13:33 +0100

    add JS folde

---
+++ exercise/js/variables.js
@@ -0,0 +1,29 @@
+// // Task 1 declaring, naming and copying
+// let admin, 
+//     name = "John";
+// admin = name;
+// alert(admin);
+
+// // Task 2 naming convention right 
+// let ourPlanetName = "earth";
+// let currenet_user;
+
+// // We generally use upper case for constants that are “hard-coded”. Or, in other words, 
+// // when the value is known prior to execution and directly written into the code.
+
+// let s = "my" + "string";
+// alert(s); // mystring
+
+// // NUMBERS
+// // There are many operators in JavaScript. Every operator has a corresponding precedence number. 
+// // The one with the larger number executes first. If the precedence is the same, the execution order is from left to right.
+
+let a = 1, b = 1;
+
+let c = ++a; // ?
+let d = b++; // ?
+
+alert(a);
+alert(b);
+alert(c);
+alert(d);
