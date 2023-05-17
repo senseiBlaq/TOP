84ad91bd1a7d643d2e4473897abd642a3e2ced33
sensei_blaq <jnr.amamoo@gmail.com>
2023-05-17 17:18:21 +0100

    other exercises

--- exercise/js/variables.js
+++ exercise/js/variables.js
@@ -1,17 +1,17 @@
 // // Task 1 declaring, naming and copying
-// let admin, 
-//     name = "John";
-// admin = name;
+let admin, 
+    name = "John";
+admin = name;
 // alert(admin);
 
 // // Task 2 naming convention right 
-// let ourPlanetName = "earth";
-// let currenet_user;
+let ourPlanetName = "earth";
+let currenet_user;
 
 // // We generally use upper case for constants that are “hard-coded”. Or, in other words, 
 // // when the value is known prior to execution and directly written into the code.
 
-// let s = "my" + "string";
+let s = "my" + "string";
 // alert(s); // mystring
 
 // // NUMBERS
@@ -20,10 +20,18 @@
 
 let a = 1, b = 1;
 
-let c = ++a; // ?
-let d = b++; // ?
+let c = ++a; // prefix incremental does the operation first and updates the variable 
+let d = b++; // postfix incremental keeps the old varible value, perfoms the operation and then update variable with new value
+// a = 2, b = 2, c = 2, d = 1
 
-alert(a);
-alert(b);
-alert(c);
-alert(d);
+//Assignment 
+console.log(45 + 23);
+console.log(d + b);
+alert((4 + 6 + 9) / 77);
+
+// Template Literal 
+const one = "Hello, ";
+const two = "how are you?";
+const joined = `${one}${two}`;
+console.log(joined); // "Hello, how are you?"
+//string methods - 
